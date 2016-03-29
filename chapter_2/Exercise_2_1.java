e2fd8c40e358851f29585fe111e87d54aee4d073
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-03-29 21:27:18 +0300

    Create Exercise_2_1.java

---
+++ chapter_2/Exercise_2_1.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author ahrytsenko
+ */
+public class Exercise_2_2 {
+    
+    public static void main(String[] args) {
+        
+        int die1 = (int)(Math.random()*6) + 1;
+        int die2 = (int)(Math.random()*6) + 1;
+        
+        System.out.printf("The first die comes up %d\n", die1);
+        System.out.printf("The second die comes up %d\n", die2);
+        System.out.printf("Your total roll is %d\n", die1+die2);
+        
+    }
+    
+}
