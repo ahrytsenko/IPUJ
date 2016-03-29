3c36f046ea797fe92c8b0afffc5e7a85377164c2
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-03-29 21:34:00 +0300

    Added files via upload

---
+++ chapter_2/Exercise_2_3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ahrytsenko
+ */
+public class Exercise_2_3 {
+    
+    public static void main(String[] args) {
+        
+        String name;
+        
+        System.out.print("What is your name? ");
+        name = TextIO.getWord();
+        
+        System.out.println("Hello, " + name + ", nice to meet you!");
+    }
+}
