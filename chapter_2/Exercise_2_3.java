6a1fa9e51112b710e5dd11e63b4656aad651c7fa
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-03-29 21:42:22 +0300

    Update Exercise_2_3.java

--- chapter_2/Exercise_2_3.java
+++ chapter_2/Exercise_2_3.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This project is a study purpose project.
+ * Everyone can use the project at his/her own reason.
+ * The project has got no warranties.
  */
 
 /**
