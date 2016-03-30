cb290acdb2b034429d4f231b6acf9ac7364603b8
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-03-30 10:42:17 +0300

    Update Exercise_2_1.java

--- chapter_2/Exercise_2_1.java
+++ chapter_2/Exercise_2_1.java
@@ -4,6 +4,23 @@
  * The project has got no warranties.
  */
 
+/*
+Exercise 2.1:
+Write a program that will print your initials to standard output in letters that are nine lines tall. 
+Each big letter should be made up of a bunch of *'s. 
+For example, if your initials were "DJE", then the output would look something like:
+
+******           *************        **********
+**    **                **            **
+**     **               **            **
+**      **              **            **
+**      **              **            ********
+**      **       **     **            **
+**     **         **    **            **
+**    **           **  **             **
+*****               ****              **********
+*/
+
 /**
  *
  * @author ahrytsenko
