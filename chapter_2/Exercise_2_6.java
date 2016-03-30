762207a5e327b8ebe072bf2578c229f9fa6a6150
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-03-30 11:54:17 +0300

    Update Exercise_2_6.java

--- chapter_2/Exercise_2_6.java
+++ chapter_2/Exercise_2_6.java
@@ -37,9 +37,24 @@ public class Exercise_2_6 {
         System.out.println("Please enter your first name and last name, separated by a space.");
         fullName = TextIO.getln();
         
+        if (fullName.indexOf(" ") != -1) {
+            firstName = fullName.substring(0, fullName.indexOf(" ")).trim();
+            lastName = fullName.substring(fullName.indexOf(" "), fullName.length()).trim();
+        }
+        else {
+            firstName = fullName.trim();
+            lastName = "";
+            
+        }
+        
         System.out.printf("Your first name is %s, which has %d characters\n", firstName, firstName.length());
         System.out.printf("Your last name is %s, which has %d characters\n", lastName, lastName.length());
-        System.out.printf("Your initials are %s\n", firstName.charAt(0).toUpperCase()+lastName.charAt(0).toUpperCase());
+        if (lastName.length() != 0) {
+            System.out.printf("Your initials are %s\n", firstName.charAt(0).toUpperCase()+lastName.charAt(0).toUpperCase());
+        }
+        else {
+            System.out.printf("Your initials are %s\n", firstName.charAt(0).toUpperCase());
+        }
         
         /**
          * Please enter your first name and last name, separated by a space.
