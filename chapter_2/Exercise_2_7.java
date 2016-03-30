b397823780743c53e5ca3792362463ae5d9342b2
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-03-30 10:37:02 +0300

    Create Exercise_2_7.java

---
+++ chapter_2/Exercise_2_7.java
@@ -0,0 +1,30 @@
+/*
+ * This project is a study purpose project.
+ * Everyone can use the project at his/her own reason.
+ * The project has got no warranties.
+ */
+ 
+ /* Exercise 2.7:
+  Suppose that a file named "testdata.txt" contains the following information: 
+  The first line of the file is the name of a student. Each of the next three lines contains an integer. 
+  The integers are the student's scores on three exams. 
+  Write a program that will read the information in the file 
+  and display (on standard output) a message that contains the name of the student 
+  and the student's average grade on the three exams. 
+  The average is obtained by adding up the individual exam grades and then dividing by the number of exams.
+ */
+
+import java.io.PrintStream;
+
+/**
+ *
+ * @author ahrytsenko
+ */
+public class Exercise_2_7 {
+    
+    public static void main(String[] args) {
+
+        // TODO
+
+    }
+}
