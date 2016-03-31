608fce53278e79715f5f9de9b318437b626b63cf
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-03-31 12:14:28 +0300

    Update Exercise_3_1.java

--- chapter_3/Exercise_3_1.java
+++ chapter_3/Exercise_3_1.java
@@ -5,21 +5,13 @@
  */
  
  /*
- Exercise 2.2:
-Write a program that simulates rolling a pair of dice. 
-You can simulate rolling one die by choosing one of the integers 1, 2, 3, 4, 5, or 6 at random. 
-The number you pick represents the number on the die after it is rolled. 
-As pointed out in Section 2.5, the expression
+Exercise 3.1:
 
-(int)(Math.random()*6) + 1
-does the computation to select a random integer between 1 and 6. 
-You can assign this value to a variable to represent one of the dice that are being rolled. 
-Do this twice and add the results together to get the total roll. 
-Your program should report the number showing on each die as well as the total roll. For example:
-
-The first die comes up 3
-The second die comes up 5
-Your total roll is 8
+How many times do you have to roll a pair of dice before they come up snake eyes? 
+You could do the experiment by rolling the dice by hand. Write a computer program that simulates the experiment. 
+The program should report the number of rolls that it makes before the dice come up snake eyes. 
+(Note: "Snake eyes" means that both dice show a value of 1.) 
+Exercise 2.2 explained how to simulate rolling a pair of dice.
 */
 
 /**
