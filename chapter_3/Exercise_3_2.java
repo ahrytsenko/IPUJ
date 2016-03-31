20a9bdddb8e3432d9e844cb5870c6b9b7b62a7e9
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-03-31 13:54:57 +0300

    Update Exercise_3_2.java

--- chapter_3/Exercise_3_2.java
+++ chapter_3/Exercise_3_2.java
@@ -1,3 +1,26 @@
+/*
+ * This project is a study purpose project.
+ * Everyone can use the project at his/her own reason.
+ * The project has got no warranties.
+ */
+ 
+ /*
+Exercise 3.2:
+Which integer between 1 and 10000 has the largest number of divisors, and how many divisors does it have? 
+Write a program to find the answers and print out the results. 
+It is possible that several integers in this range have the same, maximum number of divisors. 
+Your program only has to print out one of them. An example in Subsection 3.4.2 discussed divisors. 
+The source code for that example is CountDivisors.java.
+
+You might need some hints about how to find a maximum value. 
+The basic idea is to go through all the integers, keeping track of the largest number of divisors that you've seen so far. 
+Also, keep track of the integer that had that number of divisors.
+*/
+
+/**
+ *
+ * @author ahrytsenko
+ */
 class Exercise_3_2 {
 
 }
