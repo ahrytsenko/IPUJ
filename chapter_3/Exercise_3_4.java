e394542ddf52b7071667b22dc5c5a6c75402632b
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-04-03 20:11:38 +0300

    Update Exercise_3_4.java

--- chapter_3/Exercise_3_4.java
+++ chapter_3/Exercise_3_4.java
@@ -4,7 +4,6 @@ Write a program that reads one line of input text and breaks it up into words.
 The words should be output one per line. A word is defined to be a sequence of letters. 
 Any characters in the input that are not letters should be discarded. For example, if the user inputs the line
 He said, "That's not a good idea."
-
 then the output of the program should be
 He
 said
@@ -14,10 +13,8 @@ not
 a
 good
 idea
-
 An improved version of the program would list "that's" as a single word. 
 An apostrophe can be considered to be part of a word if there is a letter on each side of the apostrophe.
-
 To test whether a character is a letter, you might use (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'). 
 However, this only works in English and similar languages. 
 A better choice is to call the standard function Character.isLetter(ch), 
@@ -34,12 +31,39 @@ class Exercise_3_4 {
         System.out.println("Enter your string, please.");
         inputString = TextIO.getln();
         
+        getWord(inputString);
+        
     }
 
     public static void getWord(String value) {
+
+        // if value is an empty string then exit
+        value = value.trim();
+        if (value.length() == 0) return;
+
+        // seek for the first alphabet character
+        // store index of the 1st alpha char in indexFrom
+        int i = 0, indexFrom = 0;
+        while ((i < value.length()) && (!Character.isLetter(value.charAt(i)))) {
+            i++;
+        }
         
-        String aWord;
+        // if there is no alpha chars then exit
+        if (indexFrom == value.length()) return;
         
+        // save index of the 1st alpha char
+        indexFrom = i;
+        
+        // seek for the first non-alpha character
+        while ((i < value.length()) && (Character.isLetter(value.charAt(i)))) {
+            i++;
+        }
+        
+        // print extracted Word
+        System.out.println(value.substring(indexFrom, i));
+        
+        // extract next word from the string
+        getWord(value.substring(i, value.length()));
         
     }
 
