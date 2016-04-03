624de1fd76483473f97ee499bd10f66596b73b39
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-04-03 20:33:24 +0300

    Update Exercise_3_5.java

--- chapter_3/Exercise_3_5.java
+++ chapter_3/Exercise_3_5.java
@@ -1,26 +1,21 @@
 /*
- * This project is a study purpose project.
- * Everyone can use the project at his/her own reason.
- * The project has got no warranties.
- */
- 
- /*
 Exercise 3.5:
 Suppose that a file contains information about sales figures for a company in various cities. 
-Each line of the file contains a city name, followed by a colon (:) followed by the data for that city. 
+Each line of the file contains a city name, 
+followed by a colon (:) followed by the data for that city. 
 The data is a number of type double. However, for some cities, no data was available. 
 In these lines, the data is replaced by a comment explaining why the data is missing. 
-
 For example, several lines from the file might look like:
+
 San Francisco:  19887.32
 Chicago:  no report received
 New York: 298734.12
-
 Write a program that will compute and print the total sales from all the cities together. 
 The program should also report the number of cities for which data was not available. 
 The name of the file is "sales.dat".
 
-To complete this program, you'll need one fact about file input with TextIO that was not covered in Subsection 2.4.4. 
+To complete this program, you'll need one fact about file input with TextIO 
+that was not covered in Subsection 2.4.4. 
 Since you don't know in advance how many lines there are in the file, 
 you need a way to tell when you have gotten to the end of the file. 
 When TextIO is reading from a file, the function TextIO.eof() can be used to test for end of file. 
@@ -34,40 +29,29 @@ Then read the rest of the line into a variable of type String.
 Try to convert the string into a number, and use try..catch to test whether the conversion succeeds.
 */
 
-/**
- *
- * @author ahrytsenko
- */
 class Exercise_3_5 {
     
     public static void main(String[] args) {
         
-        int maxDivisorsPossessor = 0;
-        int divisorsCount = 0;
+        TextIO.readFile("d:\\sales.dat");
         
-        for (int i = START_INT; i <= STOP_INT; i++) {
-            int divisors = countDivisors(i);
-            if (divisors > divisorsCount) {
-                divisorsCount++;
-                maxDivisorsPossessor = i;
+        double sales = 0;
+        int emptyCities = 0;
+        
+        while (! TextIO.eof()) {
+            String line = TextIO.getln();
+            try {
+                sales += Double.parseDouble(line.substring(line.indexOf(':')+1, line.length()));
+            }
+            catch(Exception e) {
+                emptyCities++;
             }
         }
         
-        System.out.printf("In the range between %d and %d the largest integer which has maximum number of divisors is: ", START_INT, STOP_INT, maxDivisorsPossessor);
-        System.out.printf("The maximum number of divisors for that integer is: ", maxDivisors);
-    }
-    
-    public static int countDivisors(int number) {
-        
-        // Proceed only positive numbers
-        if (number <= 0)
-            return 0;
-            
-        int result = 0;
-        for (int i = 1; i <= number; i++) {
-            if (number % i == 0)
-                result++;
-        }
-        return result;
+        System.out.printf("The total sales is %.2f\n", sales);
+        if (emptyCities == 0) 
+            System.out.println("There was not found city without sales report.");
+        else
+            System.out.printf("There was found %d city(ies) without sales report.\n", emptyCities);
     }
 }
