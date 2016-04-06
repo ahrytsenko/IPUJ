de1a3d37181769686c1558c56c36672c4b192a43
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-04-06 21:26:32 +0300

    Update Exercise_4_3.java

--- chapter_4/Exercise_4_3.java
+++ chapter_4/Exercise_4_3.java
@@ -1,14 +1,24 @@
+/*
+ * This project is a study purpose project.
+ * Everyone can use the project at his/her own reason.
+ * The project has got no warranties.
+ */
+
+/**
+ *
+ * @author ahrytsenko
+ */
 class Exercise_4_3 {
 
     public static void main(String[] args) {
         
-        System.out.printf("It taken %d attempts to get 'Snake eyes'", rollDice(2));
+        System.out.printf("It taken %d attempts to get 'Snake eyes'\n", rollDice(2));
         
     }
     
     public static int rollDice(int desiredValue) {
         
-        if (desiredValue < 2 || desiredValue > 12) throw exception new IllegalArgumentException;
+        if (desiredValue < 2 || desiredValue > 12) throw new IllegalArgumentException();
         
         int attempts = 1;
         while ((rollDie() + rollDie()) != desiredValue) attempts++;
@@ -19,6 +29,7 @@ class Exercise_4_3 {
      * rollDie() simulates rolling of one die.
      * Returned
      * @return int - a value of rolled die
+     */
     public static int rollDie() {
         return (int)(Math.random()*6) + 1;
     }
