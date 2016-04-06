9cb5b6c91f7ed3648dc68eaa129c9aacaaa6963b
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-04-06 15:54:27 +0300

    Update Exercise_4_1.java

--- chapter_4/Exercise_4_1.java
+++ chapter_4/Exercise_4_1.java
@@ -1,3 +1,30 @@
 class Exercise_4_1 {
 
+    public static void main(String[] args) {
+        System.out.println("Enter a string, please.");
+        printCapitalized(TextIO.getln());
+    }
+    
+    public static void printCapitalized(String string) {
+        boolean firstLetter = true;
+        
+        if (string.length() == 0) return;
+        
+        for (int i = 0; i < string.length(); i++) {
+            if (Character.isLetter(string.charAt(i))) {
+                if (firstLetter) {
+                    System.out.print(string.charAt(i).toUppercase());
+                    firstLetter = false;
+                }
+                else {
+                    System.out.print(string.charAt(i);
+                }
+            }
+            else {
+                System.out.print(string.charAt(i);
+                firstLetter = true;
+            }
+        }
+        System.out.println();
+    }
 }
