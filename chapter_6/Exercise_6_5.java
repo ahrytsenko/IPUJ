792dd1262ce2b1f6f9b5b85c5bed9e33d48b44f4
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-04-20 09:51:55 +0300

    Update Exercise_6_5.java

--- chapter_6/Exercise_6_5.java
+++ chapter_6/Exercise_6_5.java
@@ -1,3 +1,25 @@
 public class Exercise_6_5 extends JPanel implements MouseListener {
 
+    public Exercise_6_5() {
+        super();
+        addMouseListener(this);
+    }
+
+    public paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
+
+    public void mousePressed(MouseAction e) { }
+    public void mouseReleased(MouseAction e) { }
+    public void mouseClicked(MouseAction e) { }
+    public void mouseEntered(MouseAction e) { }
+    public void mouseLeaved(MouseAction e) { }
+
+    public static void main(String[] args) {
+        JFrame wnd = new JFrame();
+        wnd.setLocation(100, 100);
+        wnd.setSize(320, 320);
+        wnd.setVisible(true);
+    }
+
 }
