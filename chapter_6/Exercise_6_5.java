555c6b69ee46affdb8d86f381cc8920b58535740
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-04-20 09:53:16 +0300

    Update Exercise_6_5.java

--- chapter_6/Exercise_6_5.java
+++ chapter_6/Exercise_6_5.java
@@ -13,12 +13,13 @@ public class Exercise_6_5 extends JPanel implements MouseListener {
     public void mouseReleased(MouseAction e) { }
     public void mouseClicked(MouseAction e) { }
     public void mouseEntered(MouseAction e) { }
-    public void mouseLeaved(MouseAction e) { }
+    public void mouseExited(MouseAction e) { }
 
     public static void main(String[] args) {
         JFrame wnd = new JFrame();
         wnd.setLocation(100, 100);
         wnd.setSize(320, 320);
+        wnd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         wnd.setVisible(true);
     }
 
