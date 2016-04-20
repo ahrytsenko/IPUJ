c6950359514f0d635be4ef4f9f7dc5ce7fd30ebe
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-04-20 20:56:44 +0300

    Update Exercise_6_3.java

--- chapter_6/Exercise_6_3.java
+++ chapter_6/Exercise_6_3.java
@@ -1,3 +1,13 @@
+/*
+ * This project is a study purpose project.
+ * Everyone can use the project at his/her own reason.
+ * The project has got no warranties.
+ */
+
+/**
+ *
+ * @author ahrytsenko
+ */
 
 import java.awt.*;
 import java.awt.event.*;
@@ -20,10 +30,11 @@ public class Exercise_6_3 extends JPanel implements MouseListener, MouseMotionLi
     public static void main(String[] args) {
         JFrame wnd = new JFrame("Exercise 6.3");
         Exercise_6_3 content = new Exercise_6_3();
+        //content.setPreferredSize(100, 100);
         wnd.setContentPane(content);
         wnd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         wnd.setLocation(200, 200);
-        wnd.setSize(100, 100);
+        wnd.setSize(100, 126);
         wnd.setVisible(true);
     }
     
@@ -70,7 +81,7 @@ public class Exercise_6_3 extends JPanel implements MouseListener, MouseMotionLi
         private final Point pos;
         private final Dimension size;
         
-        public Square(Color cBG, Color cFG, Point pos, Dimension size) {
+        public Die(Color cBG, Color cFG, Point pos, Dimension size) {
             colorBG = cBG;
             colorFG = cFG;
             this.pos = pos;
@@ -84,6 +95,7 @@ public class Exercise_6_3 extends JPanel implements MouseListener, MouseMotionLi
             g.drawRect(pos.x, pos.y, size.width, size.height);
             if (val > 1) {
                 g.fillOval(pos.x+size.width/4, pos.y+size.height/4, size.width/8, size.height/8);
+                g.fillOval(pos.x+size.width/4*3, pos.y+size.height/4*3, size.width/8, size.height/8);
             }
             if (val > 3) {
                 g.fillOval(pos.x+size.width/4*3, pos.y+size.height/4, size.width/8, size.height/8);
