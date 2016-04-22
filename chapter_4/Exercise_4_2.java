a7912f2674dee0f5b38978df37686a61c7bfd5b8
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-04-22 15:30:31 +0300

    Update Exercise_4_2.java

--- chapter_4/Exercise_4_2.java
+++ chapter_4/Exercise_4_2.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author agrytsenko
@@ -14,48 +8,56 @@ public class Exercise_4_2 {
     
     public static void main(String[] args) {
         
+        HexadecimalConvertion hc = new HexadecimalConvertion();
+        String str;
+        
         System.out.println(TITLE);
-        
         System.out.print("Enter Hexadecimal integer: ");
-        String str = TextIO.getln();
-        
-        long value = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (hexValue(str.charAt(i)) != -1) {
-                value = value*16 + hexValue( str.charAt(i) );
-            }
-            else {
-                System.out.println("Error was found in the given string!");
-            }
-        }
-        
-        System.out.printf("Hexadecimal '%s' is equal to '%d' in decimal.", str, value);
+        str = TextIO.getln();
+        System.out.printf("Hexadecimal '%s' is equal to '%d' in decimal.", str, hc.strHexValue(str));
         
     }
     
-    public static int hexValue(Character character) {
+    // Pravate inner class
+    private class HexadecimalConvertion {
         
-        int value;
+        public int strHexValue(String string) {
+            long value = 0;
+            for (int i = 0; i < string.length(); i++) {
+                if (charHexValue(string.charAt(i)) != -1) {
+                    value = value*16 + charHexValue( string.charAt(i) );
+                }
+                else {
+                    throw new IllegalArgumentException(string.charAt(i) + " is not a valid hexadecimal character.");
+                } // if
+            } // for
+            return value;
+        } // strHexValue
         
-        switch (Character.toUpperCase(character)) {
-            case '0': value = 0; break;
-            case '1': value = 1; break;
-            case '2': value = 2; break;
-            case '3': value = 3; break;
-            case '4': value = 4; break;
-            case '5': value = 5; break;
-            case '6': value = 6; break;
-            case '7': value = 7; break;
-            case '8': value = 8; break;
-            case '9': value = 9; break;
-            case 'A': value = 10; break;
-            case 'B': value = 11; break;
-            case 'C': value = 12; break;
-            case 'D': value = 13; break;
-            case 'E': value = 14; break;
-            case 'F': value = 15; break;
-            default: value =-10;
-        }
-        return value;
+        public int charHexValue(Character character) {
+            int value;
+            
+            switch (Character.toUpperCase(character)) {
+                case '0': value = 0; break;
+                case '1': value = 1; break;
+                case '2': value = 2; break;
+                case '3': value = 3; break;
+                case '4': value = 4; break;
+                case '5': value = 5; break;
+                case '6': value = 6; break;
+                case '7': value = 7; break;
+                case '8': value = 8; break;
+                case '9': value = 9; break;
+                case 'A': value = 10; break;
+                case 'B': value = 11; break;
+                case 'C': value = 12; break;
+                case 'D': value = 13; break;
+                case 'E': value = 14; break;
+                case 'F': value = 15; break;
+                default: value =-1;
+            } // switch
+            return value;
+        } // charHexValue
     }
+    
 }
