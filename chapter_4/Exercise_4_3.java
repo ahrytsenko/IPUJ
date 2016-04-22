951033cb391c706eea8ada9f686792779e7c4ff7
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-04-22 16:02:03 +0300

    Update Exercise_4_3.java

--- chapter_4/Exercise_4_3.java
+++ chapter_4/Exercise_4_3.java
@@ -12,25 +12,55 @@ class Exercise_4_3 {
 
     public static void main(String[] args) {
         
-        System.out.printf("It taken %d attempts to get 'Snake eyes'\n", rollDice(2));
+        System.out.printf("It taken %d attempts to get 'Snake eyes'\n", (new Dice(2)).rollDice(2));
         
     }
+
+    private class Die {
+
+        /**
+         * rollDie() simulates rolling of one die.
+         * Returned
+         * @return int - a value of rolled die
+         */
+        public int rollDie() {
+            return (int)(Math.random()*6) + 1;
+        } // rollDie
+        
+    } // class Die
     
-    public static int rollDice(int desiredValue) {
+    private class Dice extend Die {
+        private int diceAmount;
         
-        if (desiredValue < 2 || desiredValue > 12) throw new IllegalArgumentException();
+        /**
+         * Creates a set of diceAmount dice
+         * @param diceAmount - int, amount of dice in a set
+         */
+        public Dice(int diceAmount) {
+            if (diceAmount < 1) throw new IllegalArgumentException("Number of dice shoud be at least 1");
+            this.diceAmount = diceAmount;
+        }
         
-        int attempts = 1;
-        while ((rollDie() + rollDie()) != desiredValue) attempts++;
-        return attempts;
-    }
-    
-    /**
-     * rollDie() simulates rolling of one die.
-     * Returned
-     * @return int - a value of rolled die
-     */
-    public static int rollDie() {
-        return (int)(Math.random()*6) + 1;
-    }
+        /**
+         * rollDice simulates folling a set of dice until the total score is not equal to desiredValue
+         * @param desiredValue - int, desired value of total score of a set of dice
+         * @return - int, amount of attempts have been taken to get desiredValue
+         * @trouw IllegalArgumentException - in case desiredValue is out of possible values
+         */
+        public int rollDice(int desiredValue) {
+            
+            if (desiredValue < diceAmount || desiredValue > diceAmount*6) throw new IllegalArgumentException();
+            
+            int attempts = 0;
+            do {
+                int value = 0;
+                for (int i = 0; i < diceAmount; i++) value += rollDie();
+                attempts++
+            } while (value != desiredValue);
+            
+            return attempts;
+        }
+
+    } // class Dice
+
 }
