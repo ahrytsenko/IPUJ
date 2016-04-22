5ce21c08ea479999c6ac5a43b038363a2d193c20
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-04-22 16:06:47 +0300

    Create Die.java

---
+++ chapter_4/Die.java
@@ -0,0 +1,12 @@
+public class Die {
+
+    /**
+     * rollDie() simulates rolling of one die.
+     * Returned
+     * @return int - a value of rolled die
+     */
+    public int rollDie() {
+        return (int)(Math.random()*6) + 1;
+    } // rollDie
+    
+} // class Die
