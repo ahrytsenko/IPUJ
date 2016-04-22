ab5a7fe79c7c07ff048aebb65b3d7eaf61d7da09
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-04-22 16:20:40 +0300

    Update Exercise_4_4.java

--- chapter_4/Exercise_4_4.java
+++ chapter_4/Exercise_4_4.java
@@ -12,68 +12,33 @@ public class Exercise_4_4 {
     public static final int MAX_ATTEMPTS = 10000;
     
     public static void main(String[] args) {
+
+        DiceExperiment de = new DiceExperiment(MAX_ATTEMPTS);
+        
         System.out.println("Total On Dice     Average Number of Rolls");
         System.out.println("-------------     -----------------------");
         for (int i = 2; i <= 12; i++) 
             System.out.printf("%8d %22.4f\n", i, takeExperiment(MAX_ATTEMPTS, i));
     }
 
-    private class
     
-    public static double takeExperiment(int attempts, int desiredValue) {
+}
+
+public class DiceExperiment extend Dice {
+    
+    private int attempts; 
+
+    public DiceExperiment() { this(10, 2); }
+    
+    public DiceExperiment(int appempts, int diceAmount)
+
+    public double takeExperiment(int attempts, int desiredValue) {
         double average = 0;
         for (int j = 0; j < attempts; j++) {
             average += (double)rollDice(desiredValue) / attempts;
         }
         return average;
     }
-    
-    private class Die {
 
-        /**
-         * rollDie() simulates rolling of one die.
-         * Returned
-         * @return int - a value of rolled die
-         */
-        public int rollDie() {
-            return (int)(Math.random()*6) + 1;
-        } // rollDie
-        
-    } // class Die
-    
-    private class Dice extend Die {
-        private int diceAmount;
-        
-        /**
-         * Creates a set of diceAmount dice
-         * @param diceAmount - int, amount of dice in a set
-         */
-        public Dice(int diceAmount) {
-            if (diceAmount < 1) throw new IllegalArgumentException("Number of dice shoud be at least 1");
-            this.diceAmount = diceAmount;
-        }
-        
-        /**
-         * rollDice simulates folling a set of dice until the total score is not equal to desiredValue
-         * @param desiredValue - int, desired value of total score of a set of dice
-         * @return - int, amount of attempts have been taken to get desiredValue
-         * @trouw IllegalArgumentException - in case desiredValue is out of possible values
-         */
-        public int rollDice(int desiredValue) {
-            
-            if (desiredValue < diceAmount || desiredValue > diceAmount*6) throw new IllegalArgumentException();
-            
-            int attempts = 0;
-            do {
-                int value = 0;
-                for (int i = 0; i < diceAmount; i++) value += rollDie();
-                attempts++
-            } while (value != desiredValue);
-            
-            return attempts;
-        }
-
-    } // class Dice
-    
     
 }
