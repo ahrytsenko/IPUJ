90b35571a5c9f8a58008ad0fd428924db53ef726
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-04-22 15:13:24 +0300

    Update Exercise_4_1.java

--- chapter_4/Exercise_4_1.java
+++ chapter_4/Exercise_4_1.java
@@ -37,35 +37,6 @@ class Exercise_4_1 {
                 System.out.print(string.charAt(i));
             
             firstLetter = !Character.isLetter(string.charAt(i));
-            /*
-            // If current character is a letter
-            if (Character.isLetter(string.charAt(i))) {
-                
-                // and flag is rised
-                if (firstLetter) {
-                    
-                    // print capitalized character
-                    System.out.print(Character.toUpperCase(string.charAt(i)));
-                    // and put flag down
-                    firstLetter = false;
-                }
-                // if flag is not rised simply print che character
-                else {
-                    System.out.print(string.charAt(i));
-                }
-            }
-            
-            // if current character is not a letter
-            else {
-                
-                // print the character
-                System.out.print(string.charAt(i));
-                
-                // and rise the flag
-                firstLetter = true;
-            }
-                    
-                    */
         }
         System.out.println();
     }
