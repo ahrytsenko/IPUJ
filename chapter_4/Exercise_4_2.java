77a6c15d90c403d2d1d4dc4d7ad77e5dbddee71e
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-04-23 10:40:26 +0300

    Update Exercise_4_2.java

--- chapter_4/Exercise_4_2.java
+++ chapter_4/Exercise_4_2.java
@@ -14,50 +14,10 @@ public class Exercise_4_2 {
         System.out.println(TITLE);
         System.out.print("Enter Hexadecimal integer: ");
         str = TextIO.getln();
-        System.out.printf("Hexadecimal '%s' is equal to '%d' in decimal.", str, hc.strHexValue(str));
+        System.out.printf("Hexadecimal '%s' is equal to '%d' in decimal.\n", str, hc.strHexValue(str));
         
     }
     
     // Pravate inner class
-    private class HexadecimalConvertion {
-        
-        public int strHexValue(String string) {
-            long value = 0;
-            for (int i = 0; i < string.length(); i++) {
-                if (charHexValue(string.charAt(i)) != -1) {
-                    value = value*16 + charHexValue( string.charAt(i) );
-                }
-                else {
-                    throw new IllegalArgumentException(string.charAt(i) + " is not a valid hexadecimal character.");
-                } // if
-            } // for
-            return value;
-        } // strHexValue
-        
-        public int charHexValue(Character character) {
-            int value;
-            
-            switch (Character.toUpperCase(character)) {
-                case '0': value = 0; break;
-                case '1': value = 1; break;
-                case '2': value = 2; break;
-                case '3': value = 3; break;
-                case '4': value = 4; break;
-                case '5': value = 5; break;
-                case '6': value = 6; break;
-                case '7': value = 7; break;
-                case '8': value = 8; break;
-                case '9': value = 9; break;
-                case 'A': value = 10; break;
-                case 'B': value = 11; break;
-                case 'C': value = 12; break;
-                case 'D': value = 13; break;
-                case 'E': value = 14; break;
-                case 'F': value = 15; break;
-                default: value =-1;
-            } // switch
-            return value;
-        } // charHexValue
-    }
     
 }
