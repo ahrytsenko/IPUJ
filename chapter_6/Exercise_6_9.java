05812a21b2df065342fda50887633a322b6d0bdd
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-05-04 16:59:18 +0300

    Update Exercise_6_9.java

--- chapter_6/Exercise_6_9.java
+++ chapter_6/Exercise_6_9.java
@@ -13,7 +13,7 @@ import static javax.swing.JFrame.*;
  *
  * @author ahryts1
  */
-public class Exercise_6_9 extends JPanel implements MouseListener {
+public class Exercise_6_9 extends JPanel implements MouseListener, MouseMotionListener {
 
     private final Color BG_COLOR = Color.WHITE;
     private final Color FG_COLOR = Color.RED;
@@ -21,7 +21,8 @@ public class Exercise_6_9 extends JPanel implements MouseListener {
     private final int MAX_POINTS = 100;
     private int[] pointsX = new int[MAX_POINTS];
     private int[] pointsY = new int[MAX_POINTS];
-    private int points;
+    private int points, lastX, lastY;
+    private boolean nearStart;
 
 
     public static void main (String[] args) {
@@ -40,45 +41,87 @@ public class Exercise_6_9 extends JPanel implements MouseListener {
     
     public Exercise_6_9() {
         setBackground(BG_COLOR);
-        points = 0;
         addMouseListener(this);
+        addMouseMotionListener(this);
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        
-        if (points < 2) return;
-       
-        if (points < 3) {
-            g.setColor(BR_COLOR);
-            g.drawLine(pointsX[0], pointsY[0], pointsX[1], pointsY[1]);
-        } else {
-            g.setColor(FG_COLOR);
-            g.fillPolygon(pointsX, pointsY, points);
-            g.setColor(BR_COLOR);
-            g.drawPolygon(pointsX, pointsY, points);
+
+        switch (points) {
+            case 0: {
+                g.setColor(BR_COLOR);
+                g.fillRect(lastX, lastY, 2, 2);
+                break;
+            }
+            case 1: {
+                g.setColor(BR_COLOR);
+                g.drawLine(pointsX[0], pointsY[0], lastX, lastY);
+                break;
+            }
+            case 2: {
+                g.setColor(BR_COLOR);
+                g.drawLine(pointsX[0], pointsY[0], pointsX[1], pointsY[1]);
+                g.drawLine(pointsX[1], pointsY[1], lastX, lastY);
+                break;
+            }
+            default: {
+                g.setColor(FG_COLOR);
+                g.fillPolygon(pointsX, pointsY, points);
+                g.setColor(BR_COLOR);
+                g.drawPolygon(pointsX, pointsY, points);
+                g.setColor(BR_COLOR);
+                g.drawLine(pointsX[points-1], pointsY[points-1], lastX, lastY);
+            }
+        }
+        if (nearStart) {
+            g.setColor(Color.BLUE);
+            g.drawOval(lastX-3, lastY-3, 6, 6);
         }
     }
     
     @Override
     public void mousePressed(MouseEvent e) { 
-        if (points < MAX_POINTS) {
-            pointsX[points] = e.getX();
-            pointsY[points++] = e.getY();
-            repaint();
-        }
+        updateCoords(e);
     }
 
     @Override
     public void mouseClicked(MouseEvent e) { }
 
     @Override
-    public void mouseReleased(MouseEvent e) { }
+    public void mouseReleased(MouseEvent e) { 
+        if (points < MAX_POINTS) {
+            pointsX[points] = lastX;
+            pointsY[points++] = lastY;
+            repaint();
+            nearStart = false;
+        }
+    }
 
     @Override
     public void mouseEntered(MouseEvent e) { }
 
     @Override
     public void mouseExited(MouseEvent e) { }
+
+    @Override
+    public void mouseDragged(MouseEvent e) { 
+        updateCoords(e);
+    }
+
+    private void updateCoords(MouseEvent e) {
+        lastX = e.getX();
+        lastY = e.getY();
+        nearStart = (getDistance() < 2);
+        repaint();
+    }
+    
+    private double getDistance() {
+        if (points == 0) return Double.POSITIVE_INFINITY;
+        return Math.sqrt(Math.pow(pointsX[0]-lastX, 2) + Math.pow(pointsY[0]-lastY, 2));
+    }
+    
+    @Override
+    public void mouseMoved(MouseEvent e) { }
     
 }
